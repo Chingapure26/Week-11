c5c235253a73341985f3b7a5f4b52fb222ebd341
rosario <rosarioayyala@gmail.com>
2021-04-17 11:39:25 -0300

    Add Express and Nodemon

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require ('express');
+const app =express();
+const port = 4000;
+
+app.post('/register', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.put('/login', (req, res) => {
+    res.send('Hello World!')
+})
+  
+function handleRegister(){
+
+}
+
+funcion handleLogin (){
+
+}
