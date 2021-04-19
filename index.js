618c1e439b802327b3a57d8d282f8f67f5e9a964
rosario <rosarioayyala@gmail.com>
2021-04-18 22:27:07 -0300

    added function

--- index.js
+++ index.js
@@ -3,17 +3,39 @@ const cors = require('cors')
 const app =express();
 app.use(cors());
 const port = 4000;
-let dataUser=[];
 
 app.post('/register', handleRegister)
-
-app.put('/login', handleLogin)
   
 function handleRegister(req,res){
-    dataUser = req.body;
+ 
+    const UserRegist = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    };
+    console.log(UserRegist);
     res.json({result: 'Data sent'});
+    
 }
 
 function handleLogin (){
-
+    
+    const UserLogin = {
+        email: req.body.email,
+        password: req.body.password,
+    };
+    if (UserLogin.email && UserLogin.password){
+        console.log("ok");
+        return res.json({result:'User data found successfully'});
+    }else{
+        return res.status(400).json({message:'User data does not found'});
+    }
 }
+
+
+app.put('/login', handleLogin)
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
